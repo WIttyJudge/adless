9c845839234327599af80d6bcd6ec7b45cb6dc79
Ruslan <kros8925@gmail.com>
2024-10-11 16:01:56 +0300

    feat: add method to validate if provided target is correct

--- internal/config/validator.go
+++ internal/config/validator.go
@@ -1,6 +1,10 @@
 package config
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"regexp"
+)
 
 var (
 	ErrNoBlocklistsProvided = errors.New("no blocklists provided")
@@ -11,5 +15,22 @@ func Validate(config *Config) error {
 		return ErrNoBlocklistsProvided
 	}
 
+	for _, blocklist := range config.Blocklists {
+		url := blocklist.Target
+		if !checkInvalidURLSymbolsUsage(url) {
+			return fmt.Errorf("invalid blocklist target provided: %s", url)
+		}
+	}
+
 	return nil
+}
+
+// checkInvalidURLSymbolsUsage checks for characters NOT allowed in URL
+func checkInvalidURLSymbolsUsage(url string) bool {
+	matched, err := regexp.MatchString("[^a-zA-Z0-9:/?&%=~._()-;]", url)
+	if err != nil {
+		return true
+	}
+
+	return !matched
 }
